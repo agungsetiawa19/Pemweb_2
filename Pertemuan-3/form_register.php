9e78fe50154c06b0e2a6d16eb2e46a25f69330e0
agungsetiawa19 <muha22265si@studen.nurulfikri.ac.id>
2023-03-08 13:54:06 +0700

    pertemuan3

--- Pertemuan-3/form_register.php
+++ Pertemuan-3/form_register.php
@@ -5,10 +5,76 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Registrasi</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
     <div>
         <h2>Form Registrasi IT Club</h2>
+        <hr>
+
+<form>
+  <div class="form-group row">
+    <label for="NIM" class="col-4 col-form-label">NIM</label> 
+    <div class="col-8">
+      <input id="NIM" name="NIM" placeholder="Masukkan NIM" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama</label> 
+    <div class="col-8">
+      <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Jenis Klamin</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="gender" id="gender_0" type="radio" class="custom-control-input" value="Laki-Laki"> 
+        <label for="gender_0" class="custom-control-label">Laki-Laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="gender" id="gender_1" type="radio" class="custom-control-input" value="Prempuan"> 
+        <label for="gender_1" class="custom-control-label">Prempuan</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="Matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="Matkul" name="Matkul" class="custom-select">
+        <option value="rabbit">Rabbit</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Skill Programming</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill" id="skill_0" type="checkbox" class="custom-control-input" value="rabbit"> 
+        <label for="skill_0" class="custom-control-label">Rabbit</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="domisili" class="col-4 col-form-label">Tempat Domisili</label> 
+    <div class="col-8">
+      <select id="domisili" name="domisili" class="custom-select">
+        <option value="rabbit">Rabbit</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <input id="email" name="email" placeholder="Masukkan Email" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
     </div>
 </body>
 </html>
