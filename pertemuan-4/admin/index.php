61249e673787ecbf229eeb0a5bb0949a9c951cf0
agungsetiawa19 <muha22265si@studen.nurulfikri.ac.id>
2023-03-22 00:03:46 +0700

    Tugas-4

---
+++ pertemuan-4/admin/index.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'templates/header.php';
+include_once 'templates/topbar.php';
+include_once 'templates/seiber.php';
+?>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">SELAMET DATANG DI HALAMAN ADMIN.</h1>
+        
+    </div>
+</main>
+
+<?php
+include_once 'templates/footer.php';
+?>
+
