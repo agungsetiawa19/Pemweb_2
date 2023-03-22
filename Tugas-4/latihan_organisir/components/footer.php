aae482805132834bc7581714bc6907b6209da5d1
agungsetiawa19 <muha22265si@studen.nurulfikri.ac.id>
2023-03-22 22:49:33 +0700

    Tugas4

--- Tugas-4/latihan_organisir/components/footer.php
+++
@@ -1,8 +0,0 @@
-
-<footer class="footer bottom-0 text-center mt-auto bg-dark"> 
-        <div class="mx-4 mt-3">
-            <p class="small text-white">
-                CopyLeft 20232 - created with <i class="fas fa-heart text-danger"></i> by you, yes you !!
-            </p>
-        </div>
-</footer>
